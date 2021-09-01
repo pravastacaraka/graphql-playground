6c0ddd425f3619df455deb13022e270de2d91245
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2021-09-01 07:36:59 +0700

    refactor: listen to `process.env.PORT` (#2)

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ mongoose.connect(uri, {
 
 const server = new ApolloServer({ typeDefs, resolvers, plugins: [ApolloServerPluginLandingPageGraphQLPlayground()] });
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 4000).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
